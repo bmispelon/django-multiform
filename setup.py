2893773c0a21181001894d5622af798becb38eb5
Baptiste Mispelon <bmispelon@gmail.com>
2013-04-17 18:28:37 +0200

    Tweaks on setup.py.

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="django-multiform",
     version="0.0.1",
+    description="Wrap several django forms into one form-like object",
+    keywords="django, forms, multiform",
+    author="Baptiste Mispelon",
+    author_email="bmispelon@gmail.com",
     license="MIT",
     url="https://github.com/bmispelon/django-multiform",
     packages=find_packages(),
@@ -12,6 +16,8 @@ setup(
        "License :: OSI Approved :: MIT License",
        "Intended Audience :: Developers",
        "Programming Language :: Python :: 2.6",
-       "Programming Language :: Python :: 2.7"
+       "Programming Language :: Python :: 2.7",
+       "Programming Language :: Python :: 3.2",
+       "Programming Language :: Python :: 3.3",
     ]
 )
